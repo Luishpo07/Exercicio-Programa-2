8254101b72bd9287c1a2b692e51e1504d8a4ed80
Luis Henrique <luishpo@al.insper.edu.br>
2026-07-27 17:51:51 -0300

    Exercicio 6 - Questao Para Texto

--- funcoes.py
+++ funcoes.py
@@ -71,3 +71,14 @@ def sorteia_questao_inedita(questoes, nivel, questoes_sorteadas):
         if questao not in questoes_sorteadas:
             questoes_sorteadas.append(questao)
             return questao
+
+def questao_para_texto(questao, id):
+    texto = "----------------------------------------\n"
+    texto += f"QUESTAO {id}\n\n"
+    texto += f"{questao['titulo']}\n\n"
+    texto += "RESPOSTAS:\n"
+    texto += f"A: {questao['opcoes']['A']}\n"
+    texto += f"B: {questao['opcoes']['B']}\n"
+    texto += f"C: {questao['opcoes']['C']}\n"
+    texto += f"D: {questao['opcoes']['D']}"
+    return texto
